d4677e3fc5e13c406de8134bf13823b0b72e060f
Shiv Kumar Sharma <info.techieshiv@gmail.com>
2023-09-01 23:01:36 +0530

    Send email afer successful payment

--- backend/controllers/mastercard-controller.js
+++ backend/controllers/mastercard-controller.js
@@ -1,6 +1,10 @@
 const PackagePurchased = require("../models/package_purchased");
 const axios = require("axios");
 const { Buffer } = require("buffer");
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+
 const createSession = async (req, res) => {
   try {
     const data = req.body; // Assuming you're receiving the necessary data in the request body
@@ -63,6 +67,73 @@ const purchagedPackage = async (req, res) => {
     });
 
     await newRecord.save();
+
+    const message = `<table class="table-wrap is-billing">
+  <tr>
+    <th><strong>Order Id</strong></th>
+    <td>
+      <p>
+        ${body.order_id}
+      </p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Amount</strong></th>
+    <td>
+      <p>${body.amount}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Package</strong></th>
+    <td>
+      <p>${body.package_id}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Variant</strong></th>
+    <td>
+      <p>${body.variant_id}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Doctor</strong></th>
+    <td>
+      <p>${body.doctor_id}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Your Email</strong></th>
+    <td>
+      <p>${body.customer_email}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Name</strong></th>
+    <td>
+      <p>${body.customer_name}</p>
+    </td>
+  </tr>
+  <tr>
+    <th><strong>Mobile Phone</strong></th>
+    <td>
+      <p>${body.customer_phone}</p>
+    </td>
+  </tr>
+</table>`;
+
+    // send email to user
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: body.customer_email,
+      subject: "Package service is successfully purchased",
+      html: message,
+    };
+
+    const email = await sgMail.send(emailData);
+    if (email) {
+      console.log("Email sent to user");
+    }
+
     return res.status(201).json({
       message: "Package Purchased successfully",
       success: true,
